8baaf255e45da46c288af5b4e3bf713659acfe92
lizhu129 <lizhu129@gmail.com>
2022-06-19 15:17:55 +0200

    conntect react with REST API

--- src/main/java/net/springboot/student/registration/system/controller/StudentController.java
+++ src/main/java/net/springboot/student/registration/system/controller/StudentController.java
@@ -3,12 +3,14 @@ package net.springboot.student.registration.system.controller;
 import net.springboot.student.registration.system.model.Student;
 import net.springboot.student.registration.system.repository.StudentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@CrossOrigin(origins = "http://localhost:3000")
 @RestController
 @RequestMapping("/api/v1/")
 public class StudentController {
