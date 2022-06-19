cbd40e1a0fcc0be638404577a3422fdacf4bec04
lizhu129 <lizhu129@gmail.com>
2022-06-19 10:47:21 +0200

    Add List Student REST API

---
+++ src/main/java/net/springboot/student/registration/system/controller/StudentController.java
@@ -0,0 +1,25 @@
+package net.springboot.student.registration.system.controller;
+
+import net.springboot.student.registration.system.model.Student;
+import net.springboot.student.registration.system.repository.StudentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/")
+public class StudentController {
+
+    @Autowired
+    private StudentRepository studentRepository;
+
+    // Get all students
+    @GetMapping("/students")
+    public List<Student> getAllStudents() {
+        return studentRepository.findAll();
+    }
+
+}
