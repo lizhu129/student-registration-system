167453e822ae187b2b98858974f3afa691cb592f
lizhu129 <lizhu129@gmail.com>
2022-06-24 20:56:28 +0200

    add Create Student REST API

--- src/main/java/net/springboot/student/registration/system/controller/StudentController.java
+++ src/main/java/net/springboot/student/registration/system/controller/StudentController.java
@@ -3,10 +3,7 @@ package net.springboot.student.registration.system.controller;
 import net.springboot.student.registration.system.model.Student;
 import net.springboot.student.registration.system.repository.StudentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -23,5 +20,12 @@ public class StudentController {
     public List<Student> getAllStudents() {
         return studentRepository.findAll();
     }
+
+    // Create student API
+    @PostMapping("/students")
+    public Student createStudent(Student student) {
+        return studentRepository.save(student);
+    }
+
 
 }
