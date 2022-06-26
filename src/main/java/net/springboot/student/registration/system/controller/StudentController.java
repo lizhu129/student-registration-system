a258bb298074b041ef2ce7986447cd8749186f86
lizhu129 <lizhu129@gmail.com>
2022-06-26 18:46:21 +0200

    Create Get Student By Id REST API

--- src/main/java/net/springboot/student/registration/system/controller/StudentController.java
+++ src/main/java/net/springboot/student/registration/system/controller/StudentController.java
@@ -1,8 +1,10 @@
 package net.springboot.student.registration.system.controller;
 
+import net.springboot.student.registration.system.exception.ResourceNotFound;
 import net.springboot.student.registration.system.model.Student;
 import net.springboot.student.registration.system.repository.StudentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -26,6 +28,17 @@ public class StudentController {
     public Student createStudent(Student student) {
         return studentRepository.save(student);
     }
+
+    // Get student by Id
+    @GetMapping("/students/{id}")
+    public ResponseEntity<Student> getStudentById(@PathVariable long Id) {
+        Student student = studentRepository.findById(Id)
+                .orElseThrow(() -> new ResourceNotFound("Student doesn't exist with ID :" + Id));
+
+        return ResponseEntity.ok(student);
+    }
+
+
 
 
 }
